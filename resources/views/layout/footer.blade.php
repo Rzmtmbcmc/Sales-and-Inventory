dc9a8918baf95191b6001473984af58f8572f5de
Riza Mae Moico <112911889+Rzmtmbcmc@users.noreply.github.com>
2025-06-24 22:43:52 +0800

    p

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -11,7 +11,7 @@
 
                     <hr>
 
-                    <h5 class="pull-left">STI Marikina &copy;2024 All rights reserved</h5>
+                    <h5 class="pull-left"> &copy;2024 All rights reserved</h5>
 
                     <ul class="liste-unstyled pull-right">
 
