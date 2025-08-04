5a91decb7b18722b8d1d5ceaa997b72afc9067e2
EverheartMalabiga <112928158+EverheartMalabiga@users.noreply.github.com>
2025-08-04 16:26:29 +0800

    wwdw

--- resources/views/owner/products.blade.php
+++ resources/views/owner/products.blade.php
@@ -257,7 +257,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="modalTitle">Add New Product</h4>
+                    <h4 class="modal-title" id="modalTitle">Add New Products</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
