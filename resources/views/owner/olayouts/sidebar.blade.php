9d141ca026f85a702a5ad9bc6809b26ef9bdfbb7
EverheartMalabiga <112928158+EverheartMalabiga@users.noreply.github.com>
2025-08-19 17:11:29 +0800

    w

--- resources/views/owner/olayouts/sidebar.blade.php
+++ resources/views/owner/olayouts/sidebar.blade.php
@@ -111,7 +111,7 @@
 
             <li class="nav-item">
               <a href="{{route('owner.products')}}" class="nav-link{{ request()->routeIs('owner.products') ? ' active' : '' }}">
-                  <i class="nav-icon fa-boxes-stacked"></i>
+                  <i class="nav-icon fa-solid fa-boxes-stacked"></i>
                 <p>
                   Inventory
                 </p>
